2fb15e69308033f606ac64a8550857e4c44b5292
muhammed770 <mail.muhammed2002@gmail.com>
2025-01-05 10:11:58 +0400

    changed from connect/disconnecr to pair/unpair

--- init.lua
+++ init.lua
@@ -5,15 +5,15 @@
 
 
 local bluetoothDevice = "aa-bb-cc-11-22-33" -- MAC address of the Bluetooth device to connect/disconnect
-local log = hs.logger.new("SleepWatcher", "debug") 
+local log = hs.logger.new("SleepWatcher", "debug")
 
 function handleSleepAndWake(eventType)
     if eventType == hs.caffeinate.watcher.systemWillSleep then
         log.i("System is going to sleep. Attempting to disconnect Bluetooth device.")
-        hs.execute("/opt/homebrew/bin/blueutil --disconnect \"" .. bluetoothDevice .. "\"", false)
+        hs.execute("/opt/homebrew/bin/blueutil --unpair \"" .. bluetoothDevice .. "\"", false)
     elseif eventType == hs.caffeinate.watcher.systemDidWake then
         log.i("System woke up. Attempting to reconnect Bluetooth device.")
-        hs.execute("/opt/homebrew/bin/blueutil --connect \"" .. bluetoothDevice .. "\"", false)
+        hs.execute("/opt/homebrew/bin/blueutil --pair \"" .. bluetoothDevice .. "\"", false)
     end
 end
 
